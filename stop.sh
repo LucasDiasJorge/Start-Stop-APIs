029618a9df023963b0e3c2176151c4d0d2bc0b14
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-12-13 16:04:34 -0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -2,6 +2,30 @@
 
 cd /opt/$API_PATH/ || exit
 
-kill -KILL "$(cat pid.txt)"
+# Stop the Core service
+CORE_PID=$(cat /pid/core_pid.txt 2>/dev/null)
+if [ -n "$CORE_PID" ]; then
+    kill -KILL "$CORE_PID"
+    rm -f /pid/core_pid.txt
+fi
 
-rm -f pid.txt
+# Stop the Report service
+REPORT_PID=$(cat /pid/report_pid.txt 2>/dev/null)
+if [ -n "$REPORT_PID" ]; then
+    kill -KILL "$REPORT_PID"
+    rm -f /pid/report_pid.txt
+fi
+
+# Stop the Reader service
+READER_PID=$(cat /pid/reader_pid.txt 2>/dev/null)
+if [ -n "$READER_PID" ]; then
+    kill -KILL "$READER_PID"
+    rm -f /pid/reader_pid.txt
+fi
+
+# Stop the Integration service
+INTEGRATION_PID=$(cat /pid/integration_pid.txt 2>/dev/null)
+if [ -n "$INTEGRATION_PID" ]; then
+    kill -KILL "$INTEGRATION_PID"
+    rm -f /pid/integration_pid.txt
+fi
