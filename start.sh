61181f626cc8ed6fe0c36e769ef6774c8939f726
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2023-12-18 15:57:16 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,28 +1,34 @@
 #!/bin/sh
 
-cd $API_PATH || exit
+cd "$API_PATH" || exit
 
-# Crie o diretório de saída se ainda não existir
-mkdir -p /output || exit
+# Create the output directory if it doesn't exist yet
+mkdir -p output || exit
 
-# Crie o diretório de pid se ainda não existir
-mkdir -p /pid || exit
+# Create the pid directory if it doesn't exist yet
+mkdir -p pid || exit
 
-# Obtenha a data atual no formato YYYY-MM-DD
+# Get the current date in the format YYYY-MM-DD
 CURRENT_DATE=$(date +"%Y-%m-%d")
 
+# Redirect the output of each command to a log file
+>output/core/output_$CURRENT_DATE.log
+>output/report/output_$CURRENT_DATE.log
+>output/reader/output_$CURRENT_DATE.log
+>output/integration/output_$CURRENT_DATE.log
+
 # Run the Core JAR
-nohup java -jar $CORE_JAR > "/output/core/output_$CURRENT_DATE.log" 2>&1 &
-echo $! > /pid/core_pid.txt
+nohup java -jar -Xmx400m "$CORE_JAR" --server.port=8889 --spring.datasource.url=jdbc:postgresql://localhost:5432/db --spring.datasource.username=postgres --spring.datasource.password=password > "output/core/output_$CURRENT_DATE.log" 2>&1 &
+echo $! > pid/core_pid.txt
 
 # Run the Report JAR
-nohup java -jar $REPORT_JAR > "/output/report/output_$CURRENT_DATE.log" 2>&1 &
-echo $! > /pid/report_pid.txt
+nohup java -jar -Xmx1000m "$REPORT_JAR" --server.port=8890 --spring.datasource.url=jdbc:postgresql://localhost:5432/db --spring.datasource.username=postgres --spring.datasource.password=password > "output/report/output_$CURRENT_DATE.log" 2>&1 &
+echo $! > pid/report_pid.txt
 
 # Run the Reader JAR
-nohup java -jar $READER_JAR > "/output/reader/output_$CURRENT_DATE.log" 2>&1 &
-echo $! > /pid/reader_pid.txt
+nohup java -jar -Xmx400m "$READER_JAR" --server.port=8891 --spring.datasource.url=jdbc:postgresql://localhost:5432/db --spring.datasource.username=postgres --spring.datasource.password=password > "output/reader/output_$CURRENT_DATE.log" 2>&1 &
+echo $! > pid/reader_pid.txt
 
 # Run the Integration JAR
-nohup java -jar $INTEGRATION_JAR > "/output/integration/output_$CURRENT_DATE.log" 2>&1 &
-echo $! > /pid/integration_pid.txt
+nohup java -jar -Xmx400m "$INTEGRATION_JAR" --server.port=8892 --spring.datasource.url=jdbc:postgresql://localhost:5432/db --spring.datasource.username=postgres --spring.datasource.password=password > "output/integration/output_$CURRENT_DATE.log" 2>&1 &
+echo $! > pid/integration_pid.txt
