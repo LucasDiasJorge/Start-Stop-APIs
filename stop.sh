f7588ffd6d4607ef7455bc7ae4a56365534a91bf
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-01-11 12:43:17 -0300

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 
-cd $API_PATH || exit
-
 # Stop the Core service
-CORE_PID=$(cat /pid/core_pid.txt 2>/dev/null)
+CORE_PID=$(cat pid/core_pid.txt 2>/dev/null)
 if [ -n "$CORE_PID" ]; then
     kill -KILL "$CORE_PID"
-    rm -f /pid/core_pid.txt
+    rm -f pid/core_pid.txt
 fi
 
 # Stop the Report service
-REPORT_PID=$(cat /pid/report_pid.txt 2>/dev/null)
+REPORT_PID=$(cat pid/report_pid.txt 2>/dev/null)
 if [ -n "$REPORT_PID" ]; then
     kill -KILL "$REPORT_PID"
-    rm -f /pid/report_pid.txt
+    rm -f pid/report_pid.txt
 fi
 
 # Stop the Reader service
-READER_PID=$(cat /pid/reader_pid.txt 2>/dev/null)
+READER_PID=$(cat pid/reader_pid.txt 2>/dev/null)
 if [ -n "$READER_PID" ]; then
     kill -KILL "$READER_PID"
-    rm -f /pid/reader_pid.txt
+    rm -f pid/reader_pid.txt
 fi
 
 # Stop the Integration service
-INTEGRATION_PID=$(cat /pid/integration_pid.txt 2>/dev/null)
+INTEGRATION_PID=$(cat pid/integration_pid.txt 2>/dev/null)
 if [ -n "$INTEGRATION_PID" ]; then
     kill -KILL "$INTEGRATION_PID"
-    rm -f /pid/integration_pid.txt
+    rm -f pid/integration_pid.txt
 fi
