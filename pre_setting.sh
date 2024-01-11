2a0495b1d0b56d244744a14ab13dec7279c68864
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-01-11 12:33:34 -0300

    Update pre_setting.sh

--- pre_setting.sh
+++ pre_setting.sh
@@ -7,3 +7,12 @@ export READER_JAR=reader.jar
 export REPORT_JAR=report.jar
 
 export INTEGRATION_JAR=integration.jar
+
+cd "$API_PATH" || exit
+
+# Create the output directory if it doesn't exist yet
+mkdir -p output
+mkdir -p core || mkdir -p reader || mkdir -p report || mkdir -p integration
+
+# Create the pid directory if it doesn't exist yet
+mkdir -p pid || exit
